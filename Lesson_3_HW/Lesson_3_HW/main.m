295fe09b990d0fa76d92af9140a4c021e73fb1f2
Andrey Proskurin <andrewproskurin@gmail.com>
2017-08-21 19:40:38 +0300

    Последний вариант.

--- Lesson_3_HW/Lesson_3_HW/main.m
+++ Lesson_3_HW/Lesson_3_HW/main.m
@@ -10,6 +10,8 @@
 
 static NSString *const carColorKey = @"carColor";
 static NSString *const carPriceKey = @"carPrice";
+static NSString *const carModelNameKey = @"carModelName";
+static NSString *const carParametersKey = @"carParameters";
 
 NSArray *carModels() {
     
@@ -26,32 +28,18 @@ NSArray *carModels() {
     return carModelsByShops;
     
 }
-
 NSDictionary *createParametersForModel() {
     
     NSArray *carColors = @[@"Black", @"White", @"Blue", @"Silver", @"Red", @"Yellow"];
     
-    NSMutableDictionary *parametersForModel = [NSMutableDictionary dictionary];
+    NSUInteger carPrice = (100 + (CGFloat)(arc4random() % 200)) * 100;
+    NSInteger randomColor = arc4random() % (carColors.count - 1);
+    NSString *color = [carColors objectAtIndex:randomColor];
     
-    for (NSInteger i = 0; i < [carModels() count]; i++)
-    {
-        NSArray *currentBrandModels = [carModels() objectAtIndex:i];
-        
-        for (NSInteger j = 0; j < currentBrandModels.count; j++)
-        {
-            NSUInteger carPrice = (100 + (CGFloat)(arc4random() % 200)) * 100;
-            NSInteger randomColor = arc4random() % (carColors.count - 1);
-            NSString *color = [carColors objectAtIndex:randomColor];
-            
-            NSDictionary *colorAndPrice = @{carColorKey : color, carPriceKey : @(carPrice)};
-            
-            [parametersForModel setObject:colorAndPrice forKey:[currentBrandModels objectAtIndex:j]];
-        }
-    }
+    NSDictionary *colorAndPrice = @{carColorKey : color, carPriceKey : @(carPrice)};
     
-    return parametersForModel;
+    return colorAndPrice;
 }
-
 NSDictionary *createModelsForShop() {
     
     NSArray *carShops = @[@"Toyota", @"Porsche", @"Nissan", @"Mercedes", @"Honda", @"Audi", @"Lexus"];
@@ -60,12 +48,25 @@ NSDictionary *createModelsForShop() {
     
     for (NSInteger i = 0; i < carModels().count; i++)
     {
-        [modelsForShop setObject:[carModels() objectAtIndex:i] forKey:[carShops objectAtIndex:i]];
+        NSArray *carModelsArray = [carModels() objectAtIndex:i];
+        NSMutableArray *carModelsWithParameters = [NSMutableArray arrayWithArray:[carModelsArray mutableCopy]];
+        
+        for (NSInteger carModelIndex = 0; carModelIndex < carModelsWithParameters.count; carModelIndex++)
+        {
+            NSDictionary *carParameters = createParametersForModel();
+            NSString *carModelName = (NSString*)[carModelsWithParameters objectAtIndex:carModelIndex];
+            
+            NSDictionary *carModel = @{carModelNameKey : carModelName,
+                                       carParametersKey : carParameters};
+            
+            [carModelsWithParameters replaceObjectAtIndex:carModelIndex withObject:carModel];
+        }
+        
+        [modelsForShop setObject:carModelsWithParameters forKey:[carShops objectAtIndex:i]];
     }
     
     return modelsForShop;
 }
-
 void createShopsForCity() {
     
     NSArray *cityNames = @[@"Kiev", @"Kharkov", @"Lviv", @"Zaporozhye", @"Poltava", @"Vinnitsa", @"Sumy"];
@@ -80,13 +81,11 @@ void createShopsForCity() {
     NSLog(@"%@", shopsForCity);
     
 }
-
 int main(int argc, const char * argv[])
 {
     @autoreleasepool
     {
         createShopsForCity();
-        NSLog(@"%@", createParametersForModel());
     }
     return 0;
 }
